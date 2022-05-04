5f4ce8cefd079b25e8c84a0dd9e97669fd8a7a13
kiet_DEV <tuankiet2605@gmail.com>
2022-05-04 11:37:32 +0700

    Remove scripts

--- src/assets/app/mbei.js
+++
@@ -1,9 +0,0 @@
-import Mbgroupfield from './controls/mbGroupField';
-import MbSelect from './controls/mbSelect';
-
-var MBEIControls = ( ( controls ) => {
-    // console.log( "WIDGET: ", elementor );
-    elementor.addControlView( 'mb_group_field', controls.BaseData.extend( Mbgroupfield( ) ) );
-    elementor.addControlView( 'select', controls.Select.extend( MbSelect( ) ) );
-} );
-window.addEventListener( 'elementor/init', ( ) => MBEIControls( elementor.modules.controls ) )
