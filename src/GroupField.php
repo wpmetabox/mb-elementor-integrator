2987cea50ae0d9da29ba2238de4c8af205cef57e
Bao <lexuanbao161096@gmail.com>
2025-04-19 17:04:19 +0700

    Fix cloneable group field issue

--- src/GroupField.php
+++ src/GroupField.php
@@ -699,7 +699,7 @@ class GroupField {
 				ob_end_clean();
 
 				$content                                     = str_replace( [ '&#8216;', '&#8220;', '&#8221;', '&#8211;' ], [ '&#8217;', '&quot;', '&quot;', '-' ], $content );
-				$content_template['data'][ $col ]['content'] = str_replace( [ '‘', '’', "'", '"', '–' ], [ '&#8216;', '&#8217;', '&#8217;', '&quot;', '-' ], $content_template['data'][ $col ]['content'] );
+				$content_template['data'][ $col ]['content'] = str_replace( [ "'", '"', '–' ], [ '&#8217;', '&quot;', '-' ], $content_template['data'][ $col ]['content'] );
 				$content                                     = str_replace( $content_template['data'][ $col ]['content'], $value, $content );
 
 			}
