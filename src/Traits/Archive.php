574cedd26f8bfcb47c318df662388a25f29ae4bb
LyNguyen <cnttnt@gmail.com>
2026-07-08 10:19:20 +0700

    Fix not display term meta in loop

--- src/Traits/Archive.php
+++ src/Traits/Archive.php
@@ -30,13 +30,35 @@ trait Archive {
 		return $groups;
 	}
 
+	/**
+	 * Get the current term ID.
+	 * Get from $wp_query->loop_term - a global that Elementor Pro sets (see
+	 * ElementorPro\Modules\LoopBuilder\Skins\Skin_Loop_Taxonomy_Base::render_loop_content()).
+	 *
+	 * get_queried_object_id() only works on a real single term archive page
+	 */
+	private function get_current_term_id( $taxonomy ) {
+		global $wp_query;
+
+		if (
+			isset( $wp_query->loop_term )
+			&& is_object( $wp_query->loop_term )
+			&& isset( $wp_query->loop_term->term_id )
+			&& ( empty( $taxonomy ) || $wp_query->loop_term->taxonomy === $taxonomy )
+		) {
+			return (int) $wp_query->loop_term->term_id;
+		}
+
+		return get_queried_object_id();
+	}
+
 	private function handle_get_value() {
 		$key = $this->get_settings( 'key' );
 		if ( ! $key ) {
 			return null;
 		}
 		list( $taxonomy, $field_id ) = explode( ':', $key );
-		return rwmb_meta( $field_id, [ 'object_type' => 'term' ], get_queried_object_id() );
+		return rwmb_meta( $field_id, [ 'object_type' => 'term' ], $this->get_current_term_id( $taxonomy ) );
 	}
 
 	private function the_value() {
@@ -45,7 +67,7 @@ trait Archive {
 			return null;
 		}
 		list( $taxonomy, $field_id ) = explode( ':', $key );
-		rwmb_the_value( $field_id, [ 'object_type' => 'term' ], get_queried_object_id() );
+		rwmb_the_value( $field_id, [ 'object_type' => 'term' ], $this->get_current_term_id( $taxonomy ) );
 		if ( ! is_singular() ) {
 			return null;
 		}
